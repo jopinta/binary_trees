71c06a396cad31b58af43128df2f40560717c273
jopinta <tatinfa@yahoo.com>
2021-11-30 12:12:59 -0800

    6 y 7 pre & in

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_preorder -  goes through a binary tree using pre-order traversal
+ *@tree: ptr to the root node of the tree to traverse
+ *@func: ptr to a function to call for each node
+ *
+ *
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+		/** if the current node is empty*/
+    if (tree == NULL)
+        return;
+
+	/** Traverse the left subtree*/
+    binary_tree_preorder(tree->left, (*func));
+
+    /** Traverse the right subtree*/
+    binary_tree_preorder(tree->right, (*func));
+
+    func(tree->n);
+
+}
