cf1a5338b478c9c5919a23de6f6ad93fe8a9fa85
jopinta <tatinfa@yahoo.com>
2021-12-01 15:17:35 -0800

    testt for requirements

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,8 +13,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 /**alocate memory for new node */
-	binary_tree_t *temp = NULL;
-	temp = malloc(sizeof(binary_tree_t));
+	binary_tree_t *temp = malloc(sizeof(binary_tree_t));
 
 	if (!temp)
 		return (NULL);
