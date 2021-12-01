d22729ba469ad5107ebf9bd03579338fa139cfd3
jopinta <tatinfa@yahoo.com>
2021-12-01 15:00:10 -0800

    testt for requirements

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,26 +2,28 @@
 
 
 /**
-*binary_tree_node - creates a binary tree node
-*@parent:pointer to the parent node of the node to create
-*@value:value to put in the new node
+* binary_tree_node - creates a binary tree node
+* @parent:pointer to the parent node of the node to create
+* @value:value to put in the new node
 *
-*
+* Return: a pointer to the new node, or NULL on failure
 */
 
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+/**alocate memory for new node */
+	binary_tree_t *temp = NULL;
+	temp = malloc(sizeof(binary_tree_t));
 
-	binary_tree_t *temp = malloc(sizeof(binary_tree_t));
-
-	if(!temp)
+	if (!temp)
 		return (NULL);
 
-
+/**Initialize left and right children as NULL*/
 	temp->left = temp->right = NULL;
 	temp->n = value;
-        temp->parent = parent;
+	temp->parent = parent;
 
+	/**assign data to this node*/
 	return (temp);
 }
