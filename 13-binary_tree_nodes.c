f02b7e421248d22e15846299d724bb523ef7e475
jopinta <tatinfa@yahoo.com>
2021-12-02 05:06:38 -0800

    counts the nodes with at least 1 child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,11 +15,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left == NULL && tree->right != NULL)
-		return (1 + binary_tree_nodes(tree->right));
+	if (tree->right != NULL || tree->left != NULL)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	else
 
-	if (tree->left != NULL && tree->right == NULL)
-		return (1 + binary_tree_nodes(tree->left));
-
-	return (1 + binary_tree_nodes(tree->left + binary_tree_nodes(tree->right)));
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
