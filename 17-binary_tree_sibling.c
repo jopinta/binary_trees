e4681aa761fc111ef080e2a14c0da218a191128a
jopinta <tatinfa@yahoo.com>
2021-12-02 13:06:28 -0800

    finds the sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_sibling - checks if a binary tree is full
+ *@tree: is a pointer to the node to find siblings
+ *
+ *Return: if is full or 0
+ *
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left == node)
+	    return (node->parent->right);
+
+	if (node->parent->right == node)
+		return (node->parent->left);
+
+
+	return (NULL);
+
+}
